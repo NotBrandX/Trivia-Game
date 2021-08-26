375fe09e3fce8a54389191734e24959d7bafc83c
brandondoris <brandonmdoris@yahoo.com>
2021-08-26 19:43:23 -0400

    Add questions and basic CSS

--- script.js
+++ script.js
@@ -1,1 +1,107 @@
-console.log('hello world')
+// Creating questions
+const questions = [
+	{
+		question: 'What is the name of the first human civilization?',
+		choices: {
+			a: 'Persia',
+			b: 'Mesopotamia',
+			c: 'The Aztecs',
+			d: 'Pangaea',
+			answer: 'Mesopotamia',
+		},
+	},
+	{
+		question: 'When was the television invented?',
+		choices: {
+			a: '1895',
+			b: '1954',
+			c: '1927',
+			d: '1912',
+			answer: '1927',
+		},
+	},
+	{
+		question:
+			'Which US president served the shortest term of about two months because of dying from a simple cold?',
+		choices: {
+			a: 'William Henry Harrison',
+			b: 'Martin Van Buren',
+			c: 'Rutherford B. Hayes',
+			d: 'John Adams',
+			answer: 'William Henry Harrison',
+		},
+	},
+	{
+		question: 'Which astronaut was the first man to conduct a spacewalk?',
+		choices: {
+			a: 'Neil Armstrong',
+			b: 'Buzz Aldrin',
+			c: 'Elon Musk',
+			d: 'Alexei Leonov',
+			answer: 'Alexei Leonov',
+		},
+	},
+	{
+		question:
+			'What famous 5th century A.D. conqueror was known as “The Scourge of God”?',
+		choices: {
+			a: 'Hannibal',
+			b: 'Genghis Khan',
+			c: 'Attila The Hun',
+			d: 'Alexander The Great',
+			answer: 'Attila The Hun',
+		},
+	},
+	{
+		question: 'Who created the first successful printing press?',
+		choices: {
+			a: 'Thomas Edison',
+			b: 'Leonardo Da Vinci',
+			c: 'Nikola Tesla',
+			d: 'Johannes Gutenberg',
+			answer: 'Johannes Gutenberg',
+		},
+	},
+	{
+		question:
+			'There have been four eons since the Earth’s beginning. What is the name of the current eon?',
+		choices: {
+			a: 'Phanerozoic Eon',
+			b: 'Proterozoic Eon',
+			c: 'Archean Eon',
+			d: 'Hadean Eon',
+			answer: 'Phanerozoic Eon',
+		},
+	},
+	{
+		question: 'Who was the first woman in space?',
+		choices: {
+			a: 'Anna Lee Fisher',
+			b: 'Valentina Tereshkova',
+			c: 'Sally Ride',
+			d: 'Mae Jemison',
+			answer: 'Valentina Tereshkova',
+		},
+	},
+	{
+		question: 'In which country was the largest known T-Rex skeleton found?',
+		choices: {
+			a: 'Canada',
+			b: 'Mexico',
+			c: 'Australia',
+			d: 'Brazil',
+			answer: 'Canada',
+		},
+	},
+	{
+		question:
+			'The Shortest war in human civilization history was between England and Zanzibar. How many hours did it last?',
+		choices: {
+			a: 'Six days',
+			b: 'Less than an hour',
+			c: 'Five months',
+			d: 'Three weeks',
+			answer: 'Less than an hour',
+		},
+	},
+];
