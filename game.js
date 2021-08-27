4b01509e4d4f0fff01d07759f130a5a32f2ba03c
brandondoris <brandonmdoris@yahoo.com>
2021-08-27 19:46:17 -0400

    Answers load on screen now. Issue with questions

---
+++ game.js
@@ -0,0 +1,107 @@
+const questionArray = [
+	{
+		q1: 'What is the name of the first human civilization?',
+		choices: {
+			a: 'Persia',
+			b: 'Mesopotamia',
+			c: 'The Aztecs',
+			d: 'Pangaea',
+			answer: 'Mesopotamia',
+		},
+	},
+	{
+		q2: 'When was the television invented?',
+		choices: {
+			a: '1895',
+			b: '1954',
+			c: '1927',
+			d: '1912',
+			answer: '1927',
+		},
+	},
+	{
+		q3:
+			'Which US president served the shortest term of about two months because of dying from a simple cold?',
+		choices: {
+			a: 'William Henry Harrison',
+			b: 'Martin Van Buren',
+			c: 'Rutherford B. Hayes',
+			d: 'John Adams',
+			answer: 'William Henry Harrison',
+		},
+	},
+	{
+		q4: 'Which astronaut was the first man to conduct a spacewalk?',
+		choices: {
+			a: 'Neil Armstrong',
+			b: 'Buzz Aldrin',
+			c: 'Elon Musk',
+			d: 'Alexei Leonov',
+			answer: 'Alexei Leonov',
+		},
+	},
+	{
+		q5:
+			'What famous 5th century A.D. conqueror was known as “The Scourge of God”?',
+		choices: {
+			a: 'Hannibal',
+			b: 'Genghis Khan',
+			c: 'Attila The Hun',
+			d: 'Alexander The Great',
+			answer: 'Attila The Hun',
+		},
+	},
+	{
+		q6: 'Who created the first successful printing press?',
+		choices: {
+			a: 'Thomas Edison',
+			b: 'Leonardo Da Vinci',
+			c: 'Nikola Tesla',
+			d: 'Johannes Gutenberg',
+			answer: 'Johannes Gutenberg',
+		},
+	},
+	{
+		q7:
+			'There have been four eons since the Earth’s beginning. What is the name of the current eon?',
+		choices: {
+			a: 'Phanerozoic Eon',
+			b: 'Proterozoic Eon',
+			c: 'Archean Eon',
+			d: 'Hadean Eon',
+			answer: 'Phanerozoic Eon',
+		},
+	},
+	{
+		q8: 'Who was the first woman in space?',
+		choices: {
+			a: 'Anna Lee Fisher',
+			b: 'Valentina Tereshkova',
+			c: 'Sally Ride',
+			d: 'Mae Jemison',
+			answer: 'Valentina Tereshkova',
+		},
+	},
+	{
+		q9: 'In which country was the largest known T-Rex skeleton found?',
+		choices: {
+			a: 'Canada',
+			b: 'Mexico',
+			c: 'Australia',
+			d: 'Brazil',
+			answer: 'Canada',
+		},
+	},
+	{
+		q10:
+			'The Shortest war in human civilization history was between England and Zanzibar. How many hours did it last?',
+		choices: {
+			a: 'Six days',
+			b: 'Less than an hour',
+			c: 'Five months',
+			d: 'Three weeks',
+			answer: 'Less than an hour',
+		},
+	},
+];
+
